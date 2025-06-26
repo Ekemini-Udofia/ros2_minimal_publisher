254c7ec152d7c505a7930e9bce1fe8bc3d9a9b6d
Ekemini Udofia <ekeminiabasiudofia@gmail.com>
2025-06-26 16:52:58 +0100

    updated build dependencies

--- src/my_node.cpp
+++ src/my_node.cpp
@@ -9,27 +9,27 @@ using namespace std::chrono_literals;
 
 //Minimal Publisher;
 
-Class MinimalPublisher : public rclcpp::Node
+class MinimalPublisher : public rclcpp::Node
 {
+  private:
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    size_t count_;
+
+    
   public:
-    MinimalPublisher()
-    : Node("minimal_publisher"), count_(0)
+    MinimalPublisher() : Node("minimal_publisher"), count_(0)
     {
       publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
       auto timer_callback = 
         [this]() -> void {
           auto message = std_msgs::msg::String();
-          message.data = "Mallux! " + std::to_string(this->count++);
-          RCLCPP_INFO(this->gte_logger(), "Sending: '%s", message.data.c_str());
+          message.data = "Mallux! " + std::to_string(this->count_++);
+          RCLCPP_INFO(this->get_logger(), "Sending: '%s", message.data.c_str());
           this->publisher_->publish(message);
         };
         timer_ = this->create_wall_timer(500ms, timer_callback);
     }
-
-  private:
-    rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-    size_t count_;
 };
 
 
